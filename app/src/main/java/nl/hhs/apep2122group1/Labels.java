74589e26cd96127d5db5231d70ddba6d553c2199
Sander in 't Hout <sander@inthout.me>
2022-06-01 23:34:19 +0200

    Fix compile errors

--- app/src/main/java/nl/hhs/apep2122group1/Labels.java
+++ app/src/main/java/nl/hhs/apep2122group1/Labels.java
@@ -28,14 +28,6 @@ public class Labels extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(Labels.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-            }
-        });
     }
 
     @Override
