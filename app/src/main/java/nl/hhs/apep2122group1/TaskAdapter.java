6c2f3c7f034cc333c8a1813b7285291fd735518a
Majsa1 <75611648+majsa1@users.noreply.github.com>
2022-06-07 21:40:58 +0200

    Added strings and SQL notations to Task.

--- app/src/main/java/nl/hhs/apep2122group1/TaskAdapter.java
+++ app/src/main/java/nl/hhs/apep2122group1/TaskAdapter.java
@@ -40,7 +40,7 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder> {
         holder.taskStatusCb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (buttonView.isChecked()) { // check if correct
+                if (isChecked) {
                     if (tasks.get(holder.getAdapterPosition()).getCompleted() == null) {
                         tasks.get(holder.getAdapterPosition()).markTaskDone();
                     }
@@ -52,11 +52,17 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder> {
 
         holder.taskTitleTv.setText(task.getTitle());
 
-        holder.taskStatusTv.setText(completed ? "Completed: " : "Due: ");
+        holder.taskStatusTv.setText(completed ? R.string.task_adapter_completed_tv_text : R.string.task_adapter_due_tv_text);
 
         // check formatting of date:
         String taskDate = completed ? String.valueOf(task.getCompleted()) : String.valueOf(task.getDeadline());
         holder.taskDateTv.setText(taskDate);
+
+//        incorrectly also makes the same row in Done list red:
+//        if (tasks.get(holder.getAdapterPosition()).getDeadline().compareTo(LocalDateTime.now()) < 0 && tasks.get(holder.getAdapterPosition()).getCompleted() == null) {
+//            holder.taskDateTv.setTextColor(Color.RED);
+//            System.out.println("POSITION: " + task.getTitle() + tasks.get(holder.getAdapterPosition()));
+//        }
     }
 
     @Override
