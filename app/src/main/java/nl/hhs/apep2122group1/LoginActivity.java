3c5c7aefd4e18c095bc0a84797be648ef85cb95b
Sander in 't Hout <sander@inthout.me>
2022-06-08 00:13:32 +0200

    Add database stub, almost finish register/login logic

--- app/src/main/java/nl/hhs/apep2122group1/LoginActivity.java
+++ app/src/main/java/nl/hhs/apep2122group1/LoginActivity.java
@@ -5,6 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import nl.hhs.apep2122group1.database.DatabaseFactory;
+import nl.hhs.apep2122group1.models.User;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -19,7 +25,37 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void onLogin(View view) {
-        startActivity(new Intent(this, TaskListActivity.class));
+        // get fields
+        TextInputEditText usernameField = findViewById(R.id.username_et);
+        TextInputEditText passwordField = findViewById(R.id.password_et);
+
+        // get values from fields
+        String username = usernameField.getText() != null ? usernameField.getText().toString() : null;
+        String password = passwordField.getText() != null ? passwordField.getText().toString() : null;
+
+        // validate values
+        boolean error = false;
+        if (username == null || username.isEmpty()) {
+            usernameField.setError("Username cannot be empty");
+            error = true;
+        }
+        if (password == null || password.isEmpty()) {
+            passwordField.setError("Password cannot be empty");
+            error = true;
+        }
+        if (error) {
+            return;
+        }
+
+        // values ok, save to DB
+        User user = DatabaseFactory.getDatabase().getUser(username, password);
+        if (user == null) {
+            usernameField.setError("Incorrect credentials!");
+            passwordField.setError("Incorrect credentials!");
+        } else {
+            Toast.makeText(this, "Welcome!", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(this, TaskListActivity.class));
+        }
     }
 
 }
