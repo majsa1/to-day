262abc119deadcb147cc5f5c0dbc27170cc80f4e
Angeliquehhs <103029899+Angeliquehhs@users.noreply.github.com>
2022-06-08 09:14:06 +0200

    btn aangepast

--- app/src/main/java/nl/hhs/apep2122group1/AddEditActivity.java
+++ app/src/main/java/nl/hhs/apep2122group1/AddEditActivity.java
@@ -2,7 +2,9 @@ package nl.hhs.apep2122group1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class AddEditActivity extends AppCompatActivity {
 
@@ -11,4 +13,16 @@ public class AddEditActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_edit);
     }
+//    public  void makeTask(){
+//        Intent intent = new Intent(this, **waar gaat de data heen**);
+//
+//        EditText title = findViewById(R.id.edit_name_ti_text );
+//        String titleString = title.getText().toString();
+//
+//        if (titleString != null && !titleString.isEmpty()) {
+//            intent.putExtra("TITLE", titleString);
+//        }
+//        startActivity(intent);
+//
+//    }
 }
