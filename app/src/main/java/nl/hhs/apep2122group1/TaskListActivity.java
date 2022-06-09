bbd2b90d1a5a64b7b669b9176a1bc5902c4c01d3
Majsa1 <75611648+majsa1@users.noreply.github.com>
2022-06-09 12:09:03 +0200

    Updated onDelete settings bij Task & Label. Cleaned up code in TaskListActivity.

--- app/src/main/java/nl/hhs/apep2122group1/TaskListActivity.java
+++ app/src/main/java/nl/hhs/apep2122group1/TaskListActivity.java
@@ -1,20 +1,13 @@
 package nl.hhs.apep2122group1;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageButton;
 import android.widget.PopupMenu;
 
 import com.google.android.material.button.MaterialButton;
@@ -26,10 +19,7 @@ import java.util.List;
 import nl.hhs.apep2122group1.models.Task;
 
 public class TaskListActivity extends AppCompatActivity {
-    private ActivityResultLauncher<Intent> launcher;
-    private RecyclerView recyclerView;
     private TaskAdapter adapter;
-    private AlertDialog.Builder builder;
     private List<Task> tasks;
     private List<Task> sortedTasks;
     private List<Task> filteredTasks; // to be used for filtering by labels
@@ -39,130 +29,98 @@ public class TaskListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_list);
 
-        launcher = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
-                    @Override
-                    public void onActivityResult(ActivityResult result) {
-                        prepareTaskList();
-                        adapter.notifyDataSetChanged();
-                        // which materialbutton is checked on return?
-                    }
-                });
-
-        prepareTaskList();
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        // there must be a better way...
-        MaterialButton toDoBtn = findViewById(R.id.task_list_todo_mb_id);
-        getToDoTasks(toDoBtn);
-
-        prepareSort();
-        prepareLogout();
+        setTaskList();
     }
 
-    private void prepareTaskList() {
-        prepareTasks();
-        prepareList();
+//    private methods:
+
+    private void setTaskList() {
+        setTasks();
+        sortTasksByStatus(true); // tasks to do as default on start - how about when returning from (done) task detail?
+        setList();
     }
 
-    private void prepareTasks() {
+    private void setTasks() {
         tasks = Task.getDemo();
-        sortedTasks = new ArrayList<>(tasks);
-        Collections.sort(sortedTasks);
+        sortedTasks = new ArrayList<>();
     }
 
-    public void prepareList() {
-        recyclerView = findViewById(R.id.task_list_rv_id);
+    private void sortTasksByStatus(boolean toDo) {
+        sortedTasks.clear();
+        for (Task task : tasks) {
+            if (toDo && task.getCompleted() == null) {
+                sortedTasks.add(task);
+            }
+            if (!toDo && task.getCompleted() != null) {
+                sortedTasks.add(task);
+            }
+        }
+        if (toDo) {
+            Collections.sort(sortedTasks);
+        } else {
+            sortedTasks.sort(Collections.reverseOrder());
+        }
+    }
+
+    private void setList() {
+        RecyclerView recyclerView = findViewById(R.id.task_list_rv_id);
         adapter = new TaskAdapter(this, sortedTasks);
         recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
-    private void prepareLogout() {
-        builder = new AlertDialog.Builder(this);
-        ImageButton logout = findViewById(R.id.task_list_logout_btn_id);
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                builder.setTitle(R.string.task_list_logout_btn_title)
-                        .setCancelable(false) // cannot be cancelled by pressing outside of dialog
-                        .setPositiveButton(R.string.task_list_logout_btn_confirm, new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                finish();
-                            }
-                        })
-                        .setNegativeButton(R.string.task_list_logout_btn_cancel, new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                dialog.cancel();
-                            }
-                        });
-                AlertDialog alert = builder.create();
-                alert.show();
-            }
-        });
-    }
+//    public methods for views:
 
-    private void prepareSort() {
-        ImageButton sortBtn = (ImageButton) findViewById(R.id.task_list_sort_btn_id);
+    public void onSortByDeadline(View view) {
+        PopupMenu popupMenu = new PopupMenu(getApplicationContext(), view);
 
-        sortBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                PopupMenu popupMenu = new PopupMenu(getApplicationContext(), sortBtn);
-
-                popupMenu.getMenuInflater().inflate(R.menu.popup_sort, popupMenu.getMenu());
-                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                    @Override
-                    public boolean onMenuItemClick(MenuItem menuItem) {
-                        if (menuItem.getItemId() == R.id.popup_sort_ascending) {
-                            Collections.sort(sortedTasks);
-                            prepareList();
-                            return true;
-                        }
-                        if (menuItem.getItemId() == R.id.popup_sort_descending) {
-                            Collections.sort(sortedTasks, Collections.reverseOrder());
-                            prepareList();
-                            return true;
-                        }
-                        return false;
-                    }
-                });
-                popupMenu.show();
-            }
-        });
-    }
-
-    public void getToDoTasks(View view) {
-        sortedTasks.clear();
-        for (Task task : tasks) {
-            if (task.getCompleted() == null) {
-                sortedTasks.add(task);
+        popupMenu.getMenuInflater().inflate(R.menu.popup_sort, popupMenu.getMenu());
+        popupMenu.setOnMenuItemClickListener(menuItem -> {
+            if (menuItem.getItemId() == R.id.popup_sort_ascending) {
                 Collections.sort(sortedTasks);
+                setList();
+                return true;
             }
-        }
+            if (menuItem.getItemId() == R.id.popup_sort_descending) {
+                sortedTasks.sort(Collections.reverseOrder());
+                setList();
+                return true;
+            }
+            return false;
+        });
+        popupMenu.show();
+    }
+
+    public void onToDo(View view) {
+        sortTasksByStatus(true);
         adapter.notifyDataSetChanged();
     }
 
-    public void getDoneTasks(View view) {
-        sortedTasks.clear();
-        for (Task task : tasks) {
-            if (task.getCompleted() != null) {
-                sortedTasks.add(task);
-                Collections.sort(sortedTasks, Collections.reverseOrder());
-            }
-        }
+    public void onDone(View view) {
+        sortTasksByStatus(false);
         adapter.notifyDataSetChanged();
     }
 
-    public void refresh(View view) { // is there a better way? Now bound to checkbox!
+    public void onCheckChanged(View view) {
         MaterialButton toDoBtn = findViewById(R.id.task_list_todo_mb_id);
         if (toDoBtn.isChecked()) {
-            getToDoTasks(view);
+            onToDo(view);
         } else {
-            getDoneTasks(view);
+            onDone(view);
         }
     }
 
-    public void onAdd(View view) {
+    public void onAddTask(View view) {
         startActivity(new Intent(this, AddEditActivity.class));
+    }
+
+    public void onLogout(View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.task_list_logout_btn_title)
+                .setCancelable(false) // cannot be cancelled by pressing outside of dialog
+                .setPositiveButton(R.string.task_list_logout_btn_confirm, (dialog, id) -> finish())
+                .setNegativeButton(R.string.task_list_logout_btn_cancel, (dialog, id) -> dialog.cancel());
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
