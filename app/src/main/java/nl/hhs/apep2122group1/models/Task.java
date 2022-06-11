078ad6f2e3a2deab28002ab014a62ce38a6aaee3
Majsa1 <75611648+majsa1@users.noreply.github.com>
2022-06-11 22:20:58 +0200

    Fix double constructor

--- app/src/main/java/nl/hhs/apep2122group1/models/Task.java
+++ app/src/main/java/nl/hhs/apep2122group1/models/Task.java
@@ -65,14 +65,6 @@ public class Task implements Comparable<Task> {
         this.owner = owner;
         this.label = label;
     }
-    // for adding a new task:
-    public Task(String title, LocalDateTime deadline, String description, User owner, Label label) {
-        this.title = title;
-        this.deadline = deadline;
-        this.description = description;
-        this.owner = owner;
-        this.label = label;
-    }
 
     // constructor for db:
     public Task() {
